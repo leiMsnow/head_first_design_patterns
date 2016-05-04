eaf1bcccebfedcb6027bf6105622c857a8111242
ray <always_0122@163.com>
2016-05-04 22:39:16 +0800

    add description

---
+++ src/com/ray/design/patterns/singleton/ChocolateBoiler.java
@@ -0,0 +1,58 @@
+package com.ray.design.patterns.singleton;
+
+/**
+ * 巧克力工厂
+ * Created by Azure on 2016/5/4.
+ */
+public class ChocolateBoiler {
+
+    private volatile static ChocolateBoiler instance;
+
+    private boolean empty;
+    private boolean boiled;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+        System.out.println("ChocolateBoiler created!");
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (instance == null) {
+            synchronized (ChocolateBoiler.class) {
+                if (instance == null) {
+                    instance = new ChocolateBoiler();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+}
